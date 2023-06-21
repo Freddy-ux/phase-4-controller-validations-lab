9104bd5fe846d2de267a4a79f77bb1df58e7e65b
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-21 23:51:18 +0300

    DOne with Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,25 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.new(author_params)
+    if author.save
+      render json: author, status: :created
+    else
+      render json: { errors: author.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
   end
+  
+  
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  
 
   private
   
